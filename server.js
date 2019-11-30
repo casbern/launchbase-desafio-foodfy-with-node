67cdcaa76dfbead93b0359fde4e6c2ac015d2438
cassia <cascrisbern@gmail.com>
2019-11-30 06:44:56 +0000

    configuration on the server.js file

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const express = require('express')
+const nunjucks = require('nunjucks')
 
 const server = express()
 
-server.listen(3000, function(){
-  console.log('server is running')
+server.set("view engine", "html")
+
+nunjucks.configure("views", {
+  express:server
 })
 
+server.use(express.static('public'))
+
 server.get('/', function(req, res) {
-  return res.send("hello")
+  return res.render("index")
+})
+
+server.get('/about', function(req, res) {
+  return res.render("index")
+})
+
+
+server.listen(3000, function(){
+  console.log('server is running')
 })
