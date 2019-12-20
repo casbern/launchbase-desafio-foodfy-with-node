b81e4a2285e5f859ae9beabe50417f925331dd6a
cassia <cascrisbern@gmail.com>
2019-12-20 16:53:33 -0300

    addition of the page recipe

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const nunjucks = require('nunjucks')
 const server = express()
 const recipes = require("./data")
 
+server.use(express.static('public'))
+
 server.set("view engine", "njk")
 
 nunjucks.configure("views", {
@@ -12,12 +14,14 @@ nunjucks.configure("views", {
   noCache: true
 })
 
-server.use(express.static('public'))
+server.get('/', function(req, res) {  
+  let recipesFiltered = []
 
+  for(let i=0; i < 6; i++) {
+    recipesFiltered.push(recipes[i])
+  }
 
-
-server.get('/', function(req, res) {
-  return res.render("index")
+  return res.render("index", { items: recipesFiltered })
 })
 
 server.get('/about', function(req, res) {
@@ -27,6 +31,16 @@ server.get('/about', function(req, res) {
 server.get('/recipes', function(req, res) {
   return res.render("recipes", { items: recipes })
 
+})
+
+server.get('/recipes/:index', function(req, res) {
+  const { index: recipeIndex } = req.params
+
+  const recipe = recipes[recipeIndex]
+
+  if (!recipe) return res.send('Recipe not found!')
+
+  return res.render('recipe', { item: recipe })
 })
 
 
