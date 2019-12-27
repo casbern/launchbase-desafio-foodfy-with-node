045158d80fbe17e1b38d0e53628ac0cd3a998412
cassia <cascrisbern@gmail.com>
2019-12-27 11:36:23 -0300

    added bold to the selected link on the header

--- public/scripts.js
+++ public/scripts.js
@@ -1,5 +1,7 @@
 const showHide = document.querySelectorAll('h4')
 
+console.log(showHide)
+
 for(showHide of showHides) {
 
   let selector = document.querySelector('.topic-content')
@@ -23,9 +25,24 @@ for(showHide of showHides) {
       alert('cliquei no esconder')
 
     }
-
-
-
-
   })
+ }
+
+/* ==== Links ==== */
+
+function linkActive(id, pathname) {
+ //console.log(`linkActive(id=${id}, pathname=${pathname}) was called`)
+
+  const link = document.querySelector(id)
+
+  //console.log(link)
+
+  if (document.location.pathname.includes(pathname)) {
+    //console.log(`if was executed for id ${id} and pathname ${pathname}`)
+    link.classList.add("active")
+    
+  }
 }
+
+linkActive('#about', "/about")
+linkActive('#recipes', "/recipes")
