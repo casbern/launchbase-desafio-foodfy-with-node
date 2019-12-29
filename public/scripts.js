ada4719a54541ab7958de18b8fefbfd225574c93
choosy <stefan.horomnea@gmail.com>
2019-12-29 13:41:19 +0000

    make the functions showTheTopic and hideTheTopic

--- public/scripts.js
+++ public/scripts.js
@@ -1,6 +1,30 @@
 let showHides = document.querySelectorAll('h4')
 
+function showTheTopic(sectionName) {
+    console.log(`function showTheTopic(${sectionName}) has just been called`)
+
+    let topicId = 'topic-content-' + sectionName
+
+    console.log('the div we need to show has id: ' + topicId)
+    console.log('***********')
+
+}
+
+
+function hideTheTopic(sectionName) {
+    console.log(`function hideTheTopic(${sectionName}) has just been called`)
+
+    let topicId = 'topic-content-' + sectionName
+
+    console.log('the div we need to hide has id: ' + topicId)
+    console.log('***********')
+
+}
+
+
 // some test comment
+let sectionName; // this will be set as either ingredients, preparation or information
+
 
 for (showHide of showHides) {
 
@@ -16,22 +40,54 @@ for (showHide of showHides) {
       console.log(`this is the h4 innerHTML`)
       console.log(event.target.innerHTML)
 
+      console.log('this is the id of the h4')
+      let h4Id = event.target.id
+
+      /*
+        The idea is like this. We make a convention. We give these ids to h4:
+        show-hide-ingredients
+        show-hide-preparation
+        show-hide-information
+
+        We call his last bit of each id: sectionName
+
+        Why? So we identify each of these sections/topics in a unique way.
+        So the sectionNames will be:
+        - ingredients
+        - preparation
+        - information
+
+        And then, we add ids to the topic-content div like:
+        topic-content-ingredients
+        topic-content-preparation
+        topic-content-information
+
+        The idea is, when you click on on h4, you split that id (show-hide-ingredients)
+        and you take the last element: ingredients
+
+        Then you pass this to the showTheTopic(sectionName) so sectionName becomes ingredients.
+        Then we know we need to show (add css class to show) the div id id topic-content-ingredients
+
+        So we use a naming convention to make the onnection between the h4 with id="show-hide-ingredients"
+        and the div with id="topic-content-ingredients"
+
+        */
+
+      console.log(h4Id)
+
+      h4IdArray = h4Id.split('-')
+      sectionName = h4IdArray[h4IdArray.length - 1]
 
     if (event.target.innerHTML === 'MOSTRAR') {
-      //add class
 
-      //let selector = document.querySelector('.topic-content')
+        console.log('cliquei no mostrar')
 
-      console.log('cliquei no mostrar')
-
-      //   //       //style
+        showTheTopic(sectionName);
 
     } else {
-      //add class
+        console.log('cliquei no esconder')
 
-      //style
-
-      console.log('cliquei no esconder')
+        hideTheTopic(sectionName);
 
     }
 
